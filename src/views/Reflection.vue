139de533bbaee5cd07cf0436c71c6cf52bff21b2
Sebastian Swoboda <sebastian@swoboda.ch>
2020-07-07 22:47:38 +0200

    last commit

--- src/views/Reflection.vue
+++ src/views/Reflection.vue
@@ -9,6 +9,8 @@
                 Ich habe vieles über Animationen gelernt und herausgefunden das Animieren mir nicht spass macht.
                 Aber immerhin konnte ich meine Vue.js Skills verbessern. Ich denke, wir hatten viel zu wenig Zeit für
                 das Modul, weil ich wahr am Ende vom Modul sehr unter Zeitdruck und musste viel Freizeit für das Projekt opfern.
+                Was auch noch ein Challenge war, war die Seite online zu schalten. Da ich ein Vue.js Projekt gemacht habe
+                war das nicht so einfach und ich musste mehrere Stunden investieren, um die Seite online zu schalten.run
             </template>
         </Card>
     </div>
